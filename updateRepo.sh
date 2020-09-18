6504941f3595060b34df38933525e91969252c13
Romiko <romikov@gmail.com>
2020-09-18 11:24:55 +1000

    Fix repo url

--- updateRepo.sh
+++ updateRepo.sh
@@ -4,7 +4,7 @@ git diff
 git add .
 git commit -m"Helm Package"
 git push
-helm repo index --url https://romiko.github.io/helm-charts/ .
+helm repo index --url https://raw.githubusercontent.com/Romiko/helm-charts/master/ .
 git diff
 git add .
 git commit -m"helm repo index --url https://romiko.github.io/helm-charts/"
